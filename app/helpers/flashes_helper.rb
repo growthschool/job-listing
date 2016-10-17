a7915aafc8018e6ecd447bf2c4960aaab4ddd4ac
liangchaob <liangchaob@163.com>
2016-10-17 14:37:42 +0800

    add flash

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,8 +1,8 @@
 module FlashesHelper
     FLASH_CLASSES = {alert:"danger",notice:"success",warning:"warning"}.freeze
 
-    def flash_class
-        FLASH_CLASSES.fetch key.to_sym.key    
+    def flash_class(key)
+        FLASH_CLASSES.fetch key.to_sym, key    
     end
 
     def user_facing_flashes
