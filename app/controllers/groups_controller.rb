c5f0bb5ca1fc7a979612c949eb53d0d19a01f106
xbthh <hanhan_smith@hotmail.com>
2016-10-17 15:53:09 +0800

    implement groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,4 +6,10 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
+
+  def create
+    @group = Group.new(group_params)
+    @group.save
+      redirect_to groups_path
+  end
 end
