0b7bfc0a5ea6b35ab93391ac2a3cd6fa8acc69a6
Daniel <i@jiatai.ne>
2016-10-17 15:19:46 +0800

    implement simple_form

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,30 @@
 class ListsController < ApplicationController
+  before_action :authenticate_user! , only: [:new]
   def index
     @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to lists_path
+
+    else
+    render :new
+    end
+
+  end
+
+  private
+
+ def list_params
+ params.require(:list).permit(:title, :description)
   end
 end
