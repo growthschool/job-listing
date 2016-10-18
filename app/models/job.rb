33e350fbf1560da43471aaacd7f50bf633fec5d7
孙雁飞 <sunyanfei@sunyanfeideMacBook.local>
2016-10-18 21:17:46 +0800

    add

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,5 @@
 class Job < ApplicationRecord
-  validates :title,presence:true 
+  validates :title,:wage_upper_bound,:wage_lower_bound,presence:true
+
+ validates :wage_lower_bound, numericality: { greater_than: 0}
 end
