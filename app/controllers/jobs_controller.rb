68256426a067ee3a0e7a7ff19919bd3c71b941d5
David <dyz2102@gmail.com>
2016-10-18 19:52:16 +0800

    finalized step 4 forrealz...

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,7 +3,7 @@ class JobsController < ApplicationController
   #before_action :find_job_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
-      @jobs = Job.all
+      @jobs = Job.all.order("created_at DESC")
   end
 
   def new
