2b62db623e6f433542907e515a5d29472aa66147
liangchaob <liangchaob@163.com>
2016-10-18 11:48:02 +0800

    add post&get for job controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,27 @@
 class JobsController < ApplicationController
-  # job的读索引
+
+  # job的get
   def index
     @jobs = Job.all      
   end
+
+  # job的post
+  def new
+    @job = Job.new
+  end
+
+  # job的put
+  def create
+    @job = Job.new(job_params)
+    @job.save
+
+    redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title,:description)
+  end
+
 end
