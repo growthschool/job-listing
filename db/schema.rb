e52a2abcfc337670dfb853b6cdfa871f312cf0bb
Ray Gao <atlantis.ray@gmail.com>
2016-10-18 13:49:06 +0800

    add is_hidden column to jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018044954) do
+ActiveRecord::Schema.define(version: 20161018054430) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161018044954) do
     t.string   "contact_email"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.boolean  "is_hidden"
   end
 
   create_table "users", force: :cascade do |t|
