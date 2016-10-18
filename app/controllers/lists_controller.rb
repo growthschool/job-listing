452adf5d2b1afb836c3090725256b7dd4bee809d
xin <email@xxx.xxx>
2016-10-18 12:25:28 +0800

    implement lists#new

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,4 +2,29 @@ class ListsController < ApplicationController
   def index
     @lists = List.all
   end
+
+  def new
+    @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @list =  List.new(list_params)
+    @list.save
+
+       redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :description)
+  end
 end
