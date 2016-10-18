5cb59a028179dfb20ab140d799ae587b80da5694
cndaniel <tigerljt@gmail.com>
2016-10-18 11:43:23 +0800

    implement check permission.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,6 @@
 class ListsController < ApplicationController
-  before_action :authenticate_user! , only: [:new,:create]
+  before_action :authenticate_user! , only: [:new,:create, :edit, :update, :destroy]
+  before_action :find_list_and_check_permission,only:[:edit,:update,:destroy]
   def index
     @lists = List.all
   end
@@ -8,17 +9,19 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
-  def show
-    @list = List.find(params[:id])
-  end
+
 
   def edit
-    @list = List.find(params[:id])
+
+
+    if current_user !=@group.user
+      redirect_to root_pathm alert: "You have no permission"
+    end
   end
 
 
   def create
-    @list = List.new(list_params)
+
     @list.user = current_user
     if @list.save
       redirect_to lists_path
@@ -28,7 +31,7 @@ class ListsController < ApplicationController
   end
 
     def update
-      @list = List.find(list_params)
+
 
       if @list.update
         redirect_to lists_path, notice:'Update Success!'
@@ -38,7 +41,6 @@ class ListsController < ApplicationController
     end
 
       def destroy
-        @list = List.find(params[:id])
 
         if @list.destroy
           redirect_to lists_path, notice: 'Jobs deleted!'
@@ -46,8 +48,15 @@ class ListsController < ApplicationController
     end
 
   private
+  def find_list_and_check_permission
+    @list = List.find(params[:id])
 
- def list_params
- params.require(:list).permit(:title, :description)
+    if current_user != @list.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
+
+  def list_params
+    params.require(:list).permit(:title, :description)
   end
 end
