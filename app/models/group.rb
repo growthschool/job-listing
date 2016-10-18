95282deec8a5bca6ad8553a5e740d04df375c406
xbthh <hanhan_smith@hotmail.com>
2016-10-18 16:53:45 +0800

    implement step3

--- app/models/group.rb
+++
@@ -1,4 +0,0 @@
-class Group < ActiveRecord::Base
-  belongs_to :user
-  validates :title, presence: true
-end
--- db/migrate/20161018082305_add_user_id_to_jobs.rb
+++ db/migrate/20161018082305_add_user_id_to_jobs.rb
@@ -1,4 +1,4 @@
-class AddUserIdToJob < ActiveRecord::Migration[5.0]
+class AddUserIdToJobs < ActiveRecord::Migration[5.0]
   def change
     add_column :jobs, :user_id, :integer
   end
