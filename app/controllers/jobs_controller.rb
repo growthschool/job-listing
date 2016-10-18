d3b813747e72a33af72c7f8f6584624e43480ced
liangchaob <liangchaob@163.com>
2016-10-18 14:37:26 +0800

    check owner permission when access edit/update/destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
   # 添加认证
-  before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   # job的get
   def index
     @jobs = Job.all      
@@ -19,6 +19,11 @@ class JobsController < ApplicationController
   # job的编辑操作
   def edit
     @job = Job.find(params[:id])
+    # 路人重定向
+    if current_user != @job.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+
   end
 
   # job的post操作
@@ -35,6 +40,12 @@ class JobsController < ApplicationController
   # job的put操作
   def update
     @job = Job.find(params[:id])
+    # 路人重定向
+    if current_user != @job.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+
+
     if @job.update(job_params)
       redirect_to jobs_path, notice: "Update Success"
     else
@@ -45,6 +56,12 @@ class JobsController < ApplicationController
   # job的delete操作
   def destroy
     @job = Job.find(params[:id])
+    # 路人重定向
+    if current_user != @job.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+
+    
     @job.destroy
     flash[:alert] = "Job deleted"
     redirect_to jobs_path
