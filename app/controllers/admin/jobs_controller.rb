87f0e04f228c9234e6960bf27871dfd2be15242e
Zhou ZENG <zhouzeng@ZhoudeMacBook-Air.local>
2016-10-18 14:54:06 +0800

    implement admin/jobs crud

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -2,12 +2,40 @@ class Admin::JobsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :require_is_admin
 
+  def index
+    @jobs = Job.all
+  end
+
   def require_is_admin
-    if current_user.email != "hodor@ruiles.com"
+    if current_user.email != "hodor@rules.com"
       flash[:alert] = 'You are not Hodor!'
       redirect_to root_path
     end
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
 
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+
+    flash[:notice] = "HODOR Rules!"
+    redirect_to admin_jobs_path    
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+
+    flash[:alert] = "All Hail HODOR!!!"
+    redirect_to admin_jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :descripiton)
+  end
 end
