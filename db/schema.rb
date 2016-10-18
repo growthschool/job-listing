c36bf9f391e28d190943286f1bebbf75d4b96af8
张硕 <zhangshuo@zhangshuodeMacBook-Pro.local>
2016-10-18 14:55:24 +0800

    implement job CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017064042) do
+ActiveRecord::Schema.define(version: 20161018063030) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
