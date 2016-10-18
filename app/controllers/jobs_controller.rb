96485bffb29214431ba3bf61fad64c6dea74146e
xbthh <hanhan_smith@hotmail.com>
2016-10-18 12:49:28 +0800

    complet step1

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_admin! , only: [:new]
 
   def index
     @jobs = Job.all
