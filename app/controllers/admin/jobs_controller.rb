20718758f4c1ee91179b7f66d7eb4c453bb5151f
zengzheng <zengzheng2009@gmail.com>
2016-10-18 16:58:08 +0800

    create admin/jobs#edit/index/new/show

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,2 +1,40 @@
 class Admin::JobsController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to admin_jobs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.detroy
+    redirect_to admin_jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title,:description)
+  end
 end
