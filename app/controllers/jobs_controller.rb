99119b686db15ff5fe3b78cd73730e5d24e3b5fc
liangchaob <liangchaob@163.com>
2016-10-18 16:35:13 +0800

    add bound & email to job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -55,7 +55,7 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title,:description)
+    params.require(:job).permit(:title,:description,:title,:wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 
   # 检查路人权限
--- app/models/job.rb
+++ app/models/job.rb
@@ -2,5 +2,5 @@ class Job < ApplicationRecord
   # 该数据关联单user
   belongs_to :user
   # 不能为空
-  validates :title, presence: true
+  validates :title,:wage_upper_bound, :wage_lower_bound, presence: true
 end
