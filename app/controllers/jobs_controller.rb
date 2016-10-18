271b1ef24972f3ccdcaa9fa82953d2cfe0bd9512
David <dyz2102@gmail.com>
2016-10-18 11:36:54 +0800

    add validation to title

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -14,9 +14,11 @@ class JobsController < ApplicationController
 
   def create
     @job = Job.new(job_params)
-    @job.save
-
-    redirect_to jobs_path
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
 
   end
 
@@ -31,7 +33,13 @@ class JobsController < ApplicationController
 
     redirect_to jobs_path, notice: "Update Success"
   end
-  
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    flash[:alert] = "Job Deleted"
+    redirect_to jobs_path
+  end
 
   private
 
