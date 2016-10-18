3eff6468eef39a7f4d09f75204c5bbd5da70cbef
David <dyz2102@gmail.com>
2016-10-18 12:28:57 +0800

    show group's creator info

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user! , only: [:new, :create]
 
   def index
       @jobs = Job.all
@@ -15,6 +15,7 @@ class JobsController < ApplicationController
 
   def create
     @job = Job.new(job_params)
+    @job.user = current_user
     if @job.save
       redirect_to jobs_path
     else
