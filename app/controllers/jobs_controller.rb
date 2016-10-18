e9fa2c88fd052e885d170aa8e892efc4727dca23
qinfeng <q884822@gmail.com>
2016-10-18 17:20:49 +0800

    fix email

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,7 @@
 class JobsController < ApplicationController
-  before_filter :authenticate!, only:[:new,:create,:update,:edit,:destroy]
+  before_filter :authenticate_user!, only:[:new,:create,:update,:edit,:destroy]
   before_filter :require_is_admin
+
   def index
     @jobs=Job.all
   end
@@ -10,16 +11,15 @@ class JobsController < ApplicationController
   end
 
   def show
-    @job=Job.find(params[:id])
-    redirect_to admin_jobs_path
+     @job=Job.find(params[:id])
+     redirect_to admin_jobs_path
   end
 
   def require_is_admin
     if current_user.email != 'q884822@gmail.com'
-      flash[:alert]='You are not admin'
+      flash[:alert] = 'You are no admin'
       redirect_to root_path
     end
-
   end
 
   def edit
