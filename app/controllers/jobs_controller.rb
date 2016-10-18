ea6d9837d80cb9bde2a43e80c8dcc062fabb1c44
孙雁飞 <sunyanfei@sunyanfeideMacBook.local>
2016-10-18 13:07:50 +0800

    add job edit

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -13,7 +13,9 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
-  
+  def edit
+    @job = Job.find(params[:id])
+  end 
   private
   def job_params
     params.require(:job).permit(:title,:description)
