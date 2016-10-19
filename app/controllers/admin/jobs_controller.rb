2d8a8831706be53bb8d30cb25a3a77a7b55c0ed6
qinfeng <q884822@gmail.com>
2016-10-19 12:00:56 +0800

    delete #belongs_to user

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -22,7 +22,7 @@ class Admin::JobsController < ApplicationController
 
   def create
     @job = Job.new(job_params)
-    if @job.save
+    if @job.save!
       redirect_to admin_jobs_path
     else
       render :new
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,5 @@
 class Job < ApplicationRecord
-  belongs_to :user
+  #belongs_to :user
   validates :title,:description,:wage_upper_bound,:wage_lower_bound,presence:true
   validates :wage_upper_bound,:wage_lower_bound,numericality:true
 end
