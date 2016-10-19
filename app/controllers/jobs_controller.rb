cf3e89a47f97ffc2a37ab2e079ef3f70e493340b
David <dyz2102@gmail.com>
2016-10-19 14:00:55 +0800

    before nav bar after icons

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,7 +3,7 @@ class JobsController < ApplicationController
   #before_action :find_job_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
-      @jobs = Job.all.order("created_at DESC")
+      @jobs = Job.where(:is_hidden => false).order("created_at DESC")
   end
 
   def new
