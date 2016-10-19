f18628e33b61fa9144ab8d11d80d544b2ac6d378
孙雁飞 <sunyanfei@sunyanfeideMacBook.local>
2016-10-19 08:55:04 +0800

    add more column

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,6 @@
 class Job < ApplicationRecord
-  validates :title,:wage_upper_bound,:wage_lower_bound,presence:true
-
- validates :wage_lower_bound, numericality: { greater_than: 0}
+  validates :title,presence:true
+  validates :wage_upper_bound, presence: true
+  validates :wage_lower_bound, presence: true
+  validates :wage_lower_bound, numericality: { greater_than: 0}
 end
