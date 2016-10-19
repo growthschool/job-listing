6d1cea825c3b205cfafbcbd7094b95032660da22
simon <wangzd.fudan@gmail.com>
2016-10-19 12:05:38 +0800

    for step3 done

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -45,6 +45,6 @@ class JobsController < ApplicationController
     private
 
     def job_params
-      params.require(:job).permit(:title, :description)
+      params.require(:job).permit(:title, :description, :wage_upper, :wage_lower, :contact_email)
     end
 end
