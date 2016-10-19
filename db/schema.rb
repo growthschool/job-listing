6f70ab9c6eaa2d5797fc63b57dd32b2191fd7e2e
xin <email@xxx.xxx>
2016-10-19 11:35:24 +0800

    implement part 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018080323) do
+ActiveRecord::Schema.define(version: 20161019032813) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
