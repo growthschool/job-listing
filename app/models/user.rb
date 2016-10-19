50b3c0c0d458a61d0d07fc78260e5dd0a6f8f7ac
Lishuangqing <shuangqing@TIANYUANdeMacBook-Pro.local>
2016-10-19 17:46:57 +0800

    implement part 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+
+  def admin?
+     is_admin
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+      def admin?
+        email == 'xdite@growth.school'
+      end
 end
