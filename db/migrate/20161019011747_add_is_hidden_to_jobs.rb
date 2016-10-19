3841548308fa297314fae930c9104ef3681f75a1
xbthh <hanhan_smith@hotmail.com>
2016-10-19 09:26:37 +0800

    implement is_hidden button,and add routes,and add migration is_hidden_to_jobs

--- db/migrate/20161019011747_add_is_hidden_to_jobs.rb
+++ db/migrate/20161019011747_add_is_hidden_to_jobs.rb
@@ -1,4 +1,4 @@
-class AddIsHiddenToJob < ActiveRecord::Migration[5.0]
+class AddIsHiddenToJobs < ActiveRecord::Migration[5.0]
   def change
     add_column :jobs, :is_hidden, :boolean, default: false
   end
