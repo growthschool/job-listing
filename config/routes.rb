4a93cceb543a59fb8cab448227137d8ea5daa037
xin <email@xxx.xxx>
2016-10-19 15:33:21 +0800

    implement job CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :jobs
   root 'jobs#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,13 +19,6 @@ ActiveRecord::Schema.define(version: 20161018062658) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "lists", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
