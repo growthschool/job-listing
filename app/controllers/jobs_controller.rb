5600dea8630e5dc7d0a2a8047bc6ef2bac0255fe
孙雁飞 <sunyanfei@sunyanfeideMacBook.local>
2016-10-20 12:37:12 +0800

    implement step5

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,6 +3,10 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+    if @job.is_hidden
+      flash[:warning] = "This job already archieved"
+      redirect_to root_path
+    end 
   end
   def index
     @jobs = Job.where(:is_hidden => false).order("created_at DESC")
