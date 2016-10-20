7b0c6ef2e99af5f70cf185c8eaf56f46e140b78b
feiben <feiben365@gmail.com>
2016-10-20 12:45:49 +0800

    CRUD

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,44 @@
 class JobsController < ApplicationController
+
+  def index
+    @jobs = job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def create
+    @job = Job.new(job_params)
+
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path, notice:'Update Success'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+
+    @job.destroy
+    redirect_to groups_path, alert: 'Group deleted'
+  end 
 end
