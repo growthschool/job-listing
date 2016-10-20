9ee606bf6d4ad854e95f5eece755e08638d04610
xin <email@xxx.xxx>
2016-10-20 15:45:22 +0800

    implement step2-2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -41,6 +41,12 @@ class Admin::JobsController < ApplicationController
     @job.destroy
     redirect_to admin_jobs_path
   end
+  def require_is_admin
+      if !current_user.admin?
+        flash[:alert] = 'You are not admin'
+        redirect_to root_path
+      end
+    end
 
 
   private
