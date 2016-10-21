09590745aa58d33c1e4a417b4c4ba2671f914f3e
feiben <feiben365@gmail.com>
2016-10-21 14:01:52 +0800

    part5 s4

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -5,6 +5,11 @@ class Admin::JobsController < ApplicationController
 
  def show
    @job = Job.find(params[:id])
+
+   if @job.is_hidden
+     flash[:warning] = "This Job already archieved"
+     redirect_to root_path
+   end
  end
 
 
