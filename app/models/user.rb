1e78a18758fef441ae27120b3c8bc8103f6763dd
cndaniel <tigerljt@gmail.com>
2016-10-23 15:46:54 +0800

    resave model user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,4 @@ class User < ApplicationRecord
     is_admin
   end
 
-  def publish！
-    self.is_hidden = false
-    @self.save
-  end
-  def hide！
-    self.is_hidden = true
-    @self.save
-  end
 end
