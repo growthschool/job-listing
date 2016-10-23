4912076aa11239658fabf80c1e50f78bba9fd8b4
xbthh <hanhan_smith@hotmail.com>
2016-10-23 19:43:18 +0800

    完成排序功能

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,6 +4,9 @@ class Job < ApplicationRecord
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
 
+  scope :published, -> { where(is_hidden: false) }
+  scope :recent, -> { order('created_at DESC') }
+
   def publish!
     self.is_hidden = false
     self.save
@@ -13,5 +16,5 @@ class Job < ApplicationRecord
     self.is_hidden = true
     self.save
   end
-  
+
 end
