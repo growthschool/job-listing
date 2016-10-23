3e3627be4651f5d6838f5b955a0e58d76acad8dc
cndaniel <tigerljt@gmail.com>
2016-10-23 23:00:31 +0800

    implement part 6 show function iteration

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,10 +3,22 @@ before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destro
 
   def show
     @job = Job.find(params[:id])
+
+    if @job.is_hidden
+      flash[:warning] = 'This Job has already been archived'
+      redirect_to root_path
+    end
   end
 
   def index
-     @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+    @jobs = case params[:order]
+            when 'by_lower_bound'
+              Job.published.order('wage_lower_bound DESC')
+            when 'by_upper_bound'
+              Job.published.order('wage_upper_bound DESC')
+            else
+              Job.published.recent
+            end
   end
 
   def new
--- app/models/job.rb
+++ app/models/job.rb
@@ -4,8 +4,8 @@ class Job < ApplicationRecord
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than:0}
 
-
-
+  scope :published, ->{ where(is_hidden:false)}
+  scope :recent, ->{ order('created_at DESC')}
   def publish!
   self.is_hidden = false
   self.save
@@ -15,6 +15,6 @@ class Job < ApplicationRecord
   self.is_hidden = true
   self.save
   end
- 
+
 
 end
