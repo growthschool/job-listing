e145ceae046c6d9abd6854cc3867fa61c03b53c4
qinfeng <q884822@gmail.com>
2016-10-24 14:21:02 +0800

    index  add order button

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,13 +2,14 @@ class JobsController < ApplicationController
   before_filter :authenticate_user!, only:[:new,:create,:update,:edit,:destroy]
 
   def index
-    @jobs=Job.where(:is_hidden => false).order("created_at DESC")
-
-  end
-
-  def new
-    @job=Job.new
-
+    @jobs = case params[:order]
+            when 'by_lower_bound'
+              Job.where(is_hidden: false).order('wage_lower_bound DESC')
+            when 'by_upper_bound'
+              Job.where(is_hidden: false).order('wage_upper_bound DESC')
+           else
+              Job.where(is_hidden: false).order('created_at DESC')
+           end
   end
 
   def show
@@ -21,40 +22,44 @@ class JobsController < ApplicationController
 
   end
 
+  # def new
+  #   @job=Job.new
+  #
+  # end
+  #
+
+  # def edit
+  #   @job=Job.find(params[:id])
+  # end
+  #
+  # def create
+  #   @job=Job.new(job_params)
+  #
+  #   if @job.save!
+  #     redirect_to jobs_path
+  #   else
+  #     render :new
+  #   end
+  # end
+  #
+  # def update
+  #   @job=Job.find(params[:id])
+  #   if @job.update(job_params)
+  #     redirect_to jobs_path,notic:'Update Success'
+  #   else
+  #     render :edit
+  #   end
+  #
+  # end
+  #
+  # def destroy
+  #   @job=Job.find(params[:id])
+  #   @job.destroy
+  #
+  #   redirect_to jobs_path,alert: 'Job deleted'
+  # end
 
 
-  def edit
-    @job=Job.find(params[:id])
-  end
-
-  def create
-    @job=Job.new(job_params)
-
-    if @job.save!
-      redirect_to jobs_path
-    else
-      render :new
-    end
-  end
-
-  def update
-    @job=Job.find(params[:id])
-    if @job.update(job_params)
-      redirect_to jobs_path,notic:'Update Success'
-    else
-      render :edit
-    end
-
-  end
-
-  def destroy
-    @job=Job.find(params[:id])
-    @job.destroy
-
-    redirect_to jobs_path,alert: 'Job deleted'
-  end
-
-  
   private
 
   def job_params
