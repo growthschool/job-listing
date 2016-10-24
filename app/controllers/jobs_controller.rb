5c6b07389c51100b522daedb584165cbd81245de
mike562661672 <mike562661672@gmail.com>
2016-10-24 21:44:15 +0800

    第六部分

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,8 +1,16 @@
 class JobsController < ApplicationController
 before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   def index
-      @jobs = Job.where(:is_hidden => false).order("created_at DESC")
-    end
+      # @jobs = Job.all
+      @jobs = case params[:order]
+              when 'by_lower_bound'
+                 Job.where(is_hidden: false).order("wage_lower_bound DESC")
+              when 'by_upper_bound'
+                 Job.where(is_hidden: false).order("wage_upper_bound DESC")
+              else
+                 Job.where(is_hidden: false).order("created_at DESC")
+              end
+  end
 
   def new
    @job = Job.new
@@ -48,10 +56,12 @@ before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destro
        redirect_to jobs_path
      end
 
+
   private
 
   def job_params
    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 
- end
+
+end
