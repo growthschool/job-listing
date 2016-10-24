e529bbcdf34595acc0c3663cb65c035544eec1e1
William <william@WilliamtekiMacBook-Pro.local>
2016-10-25 00:17:48 +0800

    step8-count.resumes.download

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,13 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+      resources :resumes
     end
   end
 
   resources :jobs do
     resources :resumes
-  end 
+  end
 
   root 'jobs#index'
 end
