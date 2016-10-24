53ee58bd56bf17740fdd029efb380d77260c603f
张硕 <zhangshuo@zhangshuodeMacBook-Pro.local>
2016-10-24 10:52:18 +0800

    add a dropdown toggle

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,6 +3,7 @@ class Job < ApplicationRecord
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
+  scope :recent, -> { order("created_at DESC")}
   def publish!
     self.is_hidden = false
     self.save
