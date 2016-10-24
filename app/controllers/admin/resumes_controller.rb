fc12698ad5c8312a6a3ad6a910f81e32d716525b
xbthh <hanhan_smith@hotmail.com>
2016-10-24 15:14:06 +0800

    完成Admin后台查看简历

---
+++ app/controllers/admin/resumes_controller.rb
@@ -0,0 +1,13 @@
+class Admin::ResumesController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :require_is_admin
+
+  layout 'admin'
+
+  def index
+    @job = Job.find(params[:job_id])
+    @resumes = @job.resumes.order('created_at DESC')
+    # @resumes.job = @job
+    #@resumes.user = current.user
+  end
+end
