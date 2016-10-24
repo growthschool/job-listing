e070e472d21a0717c2c100e8921febe5fd6c5039
qinfeng <q884822@gmail.com>
2016-10-24 20:55:54 +0800

    resume content not null

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,4 +1,5 @@
 class ResumesController < ApplicationController
+
   def index
     @resumes = Resume.all
   end
@@ -8,9 +9,10 @@ class ResumesController < ApplicationController
   end
 
   def create
-      @job = Job.find(params[:job_id])
-      @resume = Resume.new(resume_params)
+      @job=Job.find(params[:job_id])
+      @resume=Resume.new(resume_params)
       @resume.job = @job
+      @resume.user = current_user
       @resume.user = current_user
 
       if @resume.save
--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,4 +1,5 @@
 class Resume < ApplicationRecord
+  validates :content,presence:true
   belongs_to :user
   belongs_to :job
 end
