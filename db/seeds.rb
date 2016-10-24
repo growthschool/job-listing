f50f1764d9e2375d54e95c0c57e32beb42a1d0d2
liangchaob <liangchaob@163.com>
2016-10-24 13:09:44 +0800

    use rakedbsetup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "這個種子檔會自動建立一個admin帳號, 並且創建 10 個 public jobs, 以及10個hidden jobs"
+
+create_account = User.create([email: 'idk2idk2@126.com', password: 'P@ssw0rd', password_confirmation: 'P@ssw0rd', is_admin: 'true'])
+puts "Admin account created."
+
+create_jos = for i in 1..10 do
+  Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_publish: "true",user_id: 1])
+end
+puts "10 Public jobs created."
+
+create_jos = for i in 1..10 do
+  Job.create!([title: "Job no.#{i+10}", description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_publish: "false",user_id: 1])
+end
+puts "10 Hidden jobs created."
