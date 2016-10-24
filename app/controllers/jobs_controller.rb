8cb946aca5714aac813c8399984f0de1e69825ab
孙雁飞 <sunyanfei@sunyanfeideMacBook.local>
2016-10-24 09:06:43 +0800

    add wagedescin index

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,10 +6,17 @@ class JobsController < ApplicationController
     if @job.is_hidden
       flash[:warning] = "This job already archieved"
       redirect_to root_path
-    end 
+    end
   end
   def index
-    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+    @jobs = case params[:order]
+    when 'by_lower_bound'
+      Job.where(is_hidden:false).order('wage_lower_bound DESC')
+    when 'by_upper_bound'
+      Job.where(is_hidden:false).order('wage_upper_bound DESC')
+    else
+    Job.where(is_hidden:false).order("created_at DESC")
+    end
   end
   def new
     @job = Job.new
