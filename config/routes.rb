5e3a6a4337cbbc2d8a2444909df0ede38781e111
mapuying <mapuying@gmail.com>
2016-10-25 11:30:27 +0800

    step 7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
     resources :jobs do
       member do
         post :publish
-        post :hide 
+        post :hide
       end
     end
   end
   devise_for :users
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
