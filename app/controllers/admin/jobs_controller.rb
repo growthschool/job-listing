0f95d300ecdffea287cb1ede5ed909b089023a11
liuyouzhen <liuyouzhen@gmail.com>
2016-10-25 17:03:40 +0800

    admin can see resume list

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -56,17 +56,19 @@ class Admin::JobsController < ApplicationController
       redirect_to :back
      end
 
+     def require_is_admin
+       if !current_user.admin?
+         flash[:alert] = 'You are not admin'
+         redirect_to root_path
+       end
+
+     end
+
     private
 
     def job_params
       params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
     end
 
-    def require_is_admin
-      if !current_user.admin?
-        flash[:alert] = 'You are not admin'
-        redirect_to root_path
-      end
 
-    end
 end
