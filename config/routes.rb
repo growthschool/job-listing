f2aaf0bb73da3876bb9fc3d4523ddf9bd85243fd
mike562661672 <mike562661672@gmail.com>
2016-10-25 10:03:42 +0800

    add resumes route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
    devise_for :users
-   resources :jobs
+   resources :jobs do
+     resources :resumes
+   end 
+
    root 'jobs#index'
 
    namespace :admin do
      resources :jobs do
+
        member do
          post :publish
          post :hide
